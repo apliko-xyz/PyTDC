5e1dfe1b4e3dc42f2498ac12091afe2994ac9f9d
jesusCaraball0 <jesuscaraballotsic@gmail.com>
2025-07-11 16:14:57 -0400

    removing more oracles references

--- tdc_ml/evaluator.py
+++ tdc_ml/evaluator.py
@@ -6,7 +6,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 from .utils import fuzzy_search
-from .metadata import evaluator_name, distribution_oracles
+from .metadata import evaluator_name #distribution_oracles
 
 try:
     from sklearn.metrics import (
@@ -465,8 +465,8 @@ class Evaluator:
         Returns:
             float: the evaluator output
         """
-        if self.name in distribution_oracles:
-            return self.evaluator_func(*args, **kwargs)
+        # if self.name in distribution_oracles:
+        #     return self.evaluator_func(*args, **kwargs)
         # 	#### evaluator for distribution learning, e.g., diversity, validity
         y_true = kwargs["y_true"] if "y_true" in kwargs else args[0]
         y_pred = kwargs["y_pred"] if "y_pred" in kwargs else args[1]
